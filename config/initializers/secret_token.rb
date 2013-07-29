88f6456677f719c172b33e59ec3c870ca4b65af7
Ivan Valdes R <monovaldes@gmail.com>
2013-07-29 15:42:45 -0400

    cleared folder for new approach

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Webdashboard::Application.config.secret_token = '915b3c2ae13a6504e872360195dd22577bf09907a18b78a37ce613ddf77d236e5ba9645c4f0c29965877f1731271c9419f718acd9a312a7656bb2d570b8f2498'
--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Webdashboard::Application.config.session_store :cookie_store, key: '_webdashboard_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Webdashboard::Application.config.session_store :active_record_store
